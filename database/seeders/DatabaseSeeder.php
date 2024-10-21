3c01eae531c467d22dc6cb812ff47f1423061325
Blessing Nzemo <blessingnzemojph@gmail.com>
2024-10-21 13:13:25 +0100

    Utilisation de User Unverified

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        User::factory(2)->unverified()->create();
         \App\Models\Task::factory(20)->create();
+
 
         User::factory()->create([
             'name' => 'Test User',
