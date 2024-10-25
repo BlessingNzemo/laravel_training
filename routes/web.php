aac6d9ec4471351c3477102f50dc9e65c2b911aa
Blessing Nzemo <blessingnzemojph@gmail.com>
2024-10-25 20:04:55 +0100

    Refactorisation du code

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Response;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Laravel\Prompts\Concerns\Fallback;
+use App\Http\Requests\TaskRequest;
 
 
 
@@ -27,9 +28,9 @@ Route::get('/', function () {
 });
 
 
-Route::get('/tasks', function() {
+Route::get('/tasks', function(Task $task) {
     return view('index', [
-        'tasks'=> \App\Models\Task::latest()->get(),
+        'tasks'=> Task::latest()->get(),
         // 'tasks'=> \App\Models\Task::latest()->where('completed',false)->get(),
         //\App\Models\Task::all() permet de récupérer toutes les tâches par contre latest() permet de récupérer les tâches les plus récentes
     ]);
@@ -37,8 +38,26 @@ Route::get('/tasks', function() {
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
-Route::get('/tasks/{id}', function ($id) {
-    return view( 'show', ['task'=>\App\Models\Task::findOrFail($id)]);
+Route::post('/tasks', function(TaskRequest $request) {
+    $task = Task::create([
+        $task = Task::create($request->validated())
+
+    ]);
+    return redirect()->route('tasks.show', ['task'=>$task->id])->with('success','Task created with success');
+
+})->name('tasks.store');
+
+
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view( 'edit', ['task'=>$task->id]);
+
+})->name('tasks.edit');
+
+
+
+
+Route::get('/tasks/{task}', function (Task $task) {
+    return view( 'show', ['task'=>$task]);
 
     //La commande FindOrfail est différente de find
     // findorfail cherche la ligne et l'abstient de l'erreur
@@ -79,10 +98,9 @@ Route::post('/tasks', function(Request $request) {
     $task->title = $data['title'];
     $task->description = $data['description'];
     $task->long_description = $data['long_description'];
-
     $task->save();
 
-    return redirect()->route('tasks.show', ['id'=>$task->id]);
+    return redirect()->route('tasks.show', ['task'=>$task->id])->with('success','Task created with success');
 
 
     //dd($request->all());
@@ -93,6 +111,44 @@ Route::post('/tasks', function(Request $request) {
     // ]);
     // return redirect('/tasks');
 })->name('tasks.store');
+
+
+
+
+// Route::put('/tasks/{id}/edit', function($id,Request $request) {
+
+
+//     $data = $request->validate([
+//         'title'=>'required|max:255',
+//         'description'=>'required',
+//         'long_description'=>'required',
+//     ]);
+
+//     $task = Task::findOrFail($id);
+//     $task->title = $data['title'];
+//     $task->description = $data['description'];
+//     $task->long_description = $data['long_description'];
+//     $task->save();
+
+//     return redirect()->route('tasks.show', ['id'=>$task->id])->with('success','Task updated with success !');
+
+
+//     //dd($request->all());
+//     // \App\Models\Task::create([
+//     //     'title'=>request('title'),
+//     //     'description'=>request('description'),
+//     //     'completed'=>request('completed'),
+//     // ]);
+//     // return redirect('/tasks');
+// })->name('tasks.update');
+
+Route::put('/tasks/{task}', function(Task $task,TaskRequest $request) {
+
+    $task->update($request->validated());
+    return redirect()->route('tasks.show', ['task'=>$task->id])->with('success','Task updated with success !');
+
+})->name('tasks.update');
+
 
 Route::fallback( function(){
     return 'Cette route est inexistante';
