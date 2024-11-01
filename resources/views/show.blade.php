9646deb0bc1588f411e5896571e50df99a232b2c
Blessing Nzemo <blessingnzemojph@gmail.com>
2024-11-01 14:42:58 +0100

    Utilsation des directives classes et apply pour l'injection des classes dans la vue avec Tailwind

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,33 +3,49 @@
 
 @section('content')
 
-    <p>{{ $task->description }}</p>
+    <div class="mb-4">
+        <a href="{{ route('tasks.index') }}" class="link">Back to the list</a>
+    </div>
+
+    <p class='mb-4 text-slate-700'>{{ $task->description }}</p>
     @if ($task->long_description)
-        <p>{{ $task->long_description }}</p>
+        <p class='mb-4 text-slate-700'>{{ $task->long_description }}</p>
     @endif
 
-    <p>{{ $task->created_at }}</p>
+    <p class ="mb-4 text-slate-500">Created {{ $task->created_at->diffForHumans() }} . Updated
+        {{ $task->updated_at->diffForHumans() }}</p>
     <p>{{ $task->updated_at }}</p>
 
-    <div>
+    <div class="mb-4">
+        @if ($task->completed)
+            <span class="font-medium text-green-500">Completed</span>
+        @else
+            <span class="font-medium text-red-500">Not Completed</span>
+        @endif
 
-        <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit</a>
-    </div>
+        <div class="flex gap-2">
 
-    <div>
-        <form action="{{ route('tasks.toggle-complete', ['task' => $task]) }}" method='POST'>
+                
 
-            @csrf
-            @method('PUT')
-            <button type='submit'>Mark as {{ $task->completed ? 'Incompleted' : 'Completed' }}</button>
+                <a href="{{ route('tasks.edit', ['task' => $task]) }}" class="btn">Edit</a>
 
-        </form>
-    </div>
-    <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method='POST'>
-        @csrf
-        @method('DELETE')
-        <button type='submit'>Delete</button>
 
-    </form>
+            <div>
+                <form action="{{ route('tasks.toggle-complete', ['task' => $task]) }}" method='POST'>
+
+                    @csrf
+                    @method('PUT')
+                    <button class="btn" type='submit'>Mark as
+                        {{ $task->completed ? 'Incompleted' : 'Completed' }}</button>
+
+                </form>
+            </div>
+            <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method='POST'>
+                @csrf
+                @method('DELETE')
+                <button class="btn" type='submit'>Delete</button>
+
+            </form>
+        </div>
     </div>
 @endsection
