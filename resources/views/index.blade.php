b9e7047afbe35848d00adf130b3b7686e3f4666b
Blessing Nzemo <blessingnzemojph@gmail.com>
2024-11-01 11:39:27 +0100

    Correction des bugs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,9 +2,11 @@
  @section('title', 'The List of Tasks')
 
  @section('content')
- <div>
-    <a href="{{route('tasks.create')}}">Add Task</p>
- </div>
+
+
+ <nav class="mb-4">
+    <a href="{{route('tasks.create')}}" class="font-mrdium text-gray-700 underline decoration-pink-500">Add Task</a>
+    </nav>
 @isset($name)
     {{ $name }}
 @endisset
@@ -13,14 +15,18 @@
 {{-- @if (count($tasks)) --}}
 @forelse ($tasks as $task)
     <div>
-        <a href ="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
+        <a href="{{ route('tasks.show', ['task' => $task->id]) }}"
+           @class(['font-bold', 'line-through' => $task->completed])>
+            {{ $task->title }}
+        </a>
     </div>
 
 @empty
     <div>There are no tasks</div>
+
 @endforelse
 @if ($tasks->count())
-    <nav>
+    <nav class="mb-4">
         {{ $tasks->links() }}
     </nav>
 @endif
